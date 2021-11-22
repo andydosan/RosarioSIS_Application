3166162b3144b23d710bd9c85558543cca58bc91
rseah <rseah@students.logoscambodia.org>
2021-11-22 12:30:55 +0700

    first test

--- app/src/main/java/com/example/rosariosisapplication/MainActivity.java
+++ app/src/main/java/com/example/rosariosisapplication/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.rosariosisapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //hi minchan
     }
+
+
 }
