37117c8bf18abd8b4631e5e1e3f3854624ce059a
minchan.kim <minchan.kim@students.logoscambodia.org>
2021-11-22 12:39:57 +0700

    added libraries

--- app/src/main/java/com/example/rosariosisapplication/MainActivity.java
+++ app/src/main/java/com/example/rosariosisapplication/MainActivity.java
@@ -9,6 +9,14 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import java.io.IOException;
 import java.util.Map;
 
