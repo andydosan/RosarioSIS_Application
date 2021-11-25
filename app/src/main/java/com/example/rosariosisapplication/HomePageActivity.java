495ce8fff1ff8558a7d715d4ff7e6d897eb96476
Andy Dosan <starzstorm123@gmail.com>
2021-11-25 14:09:22 +0700

    home page activity stuff

--- app/src/main/java/com/example/rosariosisapplication/HomePageActivity.java
+++ app/src/main/java/com/example/rosariosisapplication/HomePageActivity.java
@@ -2,13 +2,79 @@ package com.example.rosariosisapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class HomePageActivity extends AppCompatActivity {
+
+    TextView text;
+    String code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        text = findViewById(R.id.html);
+
+    }
+
+    private class description_webscrape extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            //With this you login and a session is created
+            Connection.Response res = null;
+            try {
+                res = Jsoup.connect("https://rosariosis.asianhope.org/")
+                        //DO NOT SCROLL RIGHT
+                        .data("username", "rseah", "password",                                                                                              "significantcookie52")
+                        .method(Connection.Method.POST)
+                        .execute();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            //This will get you cookies
+            Map<String, String> loginCookies = res.cookies();
+
+            //Here you parse the page that you want. Put the url that you see when you have logged in
+            try {
+                org.jsoup.nodes.Document doc = Jsoup.connect("https://rosariosis.asianhope.org/Modules.php?modname=misc/Portal.php")
+                        .cookies(loginCookies)
+                        .get();
+
+                code = doc.text();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+
+            text.setText(code);
+        }
     }
 }
